06c5e3e6f4bf1efcf2243c73eaaa2f823ed67474
Paul Mackinlay <paul@webotech.co.uk>
2024-08-04 09:23:48 +0100

    shutdown the executor once it's done

--- src/test/java/com/webotech/statemachine/service/TestApp.java
+++ src/test/java/com/webotech/statemachine/service/TestApp.java
@@ -5,6 +5,7 @@
 package com.webotech.statemachine.service;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TestApp extends AbstractAppService<TestAppContext> {
@@ -14,8 +15,13 @@ public class TestApp extends AbstractAppService<TestAppContext> {
   }
 
   public static void main(String[] args) {
-    TestApp testApp = new TestApp(args);
-    Executors.newSingleThreadScheduledExecutor().schedule(testApp::stop, 3, TimeUnit.SECONDS);
-    testApp.start();
+    ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    try {
+      TestApp testApp = new TestApp(args);
+      scheduledExecutorService.schedule(testApp::stop, 3, TimeUnit.SECONDS);
+      testApp.start();
+    } finally {
+      scheduledExecutorService.shutdownNow();
+    }
   }
 }
