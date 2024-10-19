59e75f3a67fd8f46f5866de65c9d2907761e5d0c
Paul Mackinlay <paul@webotech.co.uk>
2024-10-19 10:15:32 +0100

    create TODO

--- src/main/java/com/webotech/statemachine/strategy/TransitionTask.java
+++ src/main/java/com/webotech/statemachine/strategy/TransitionTask.java
@@ -25,8 +25,8 @@ public class TransitionTask<T, S> {
 
   void execute(StateEvent<S> event, GenericStateMachine<T, S> machine) {
     //TODO work out how stateEventStateMap can be null
-    Map<StateEvent<S>, State<T, S>> stateEventStateMap = states.get(machine.getCurrentState());
-    State<T, S> toState = stateEventStateMap == null ? null : stateEventStateMap.get(event);
+    // Map<StateEvent<S>, State<T, S>> stateEventStateMap = states.get(machine.getCurrentState());
+    State<T, S> toState = states.get(machine.getCurrentState()).get(event);
     if (toState == null) {
       unmappedEventHandler.accept(event, machine);
       return;
